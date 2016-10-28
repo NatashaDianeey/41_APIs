353d310708c37fd8f41d1a9c5e0a794e514e6bf6
NatashaDianeey <ndianeey@gamil.com>
2016-10-28 16:17:59 +0000

    Trello casi termiando con pequeños errores

--- Trello/js/main.js
+++ Trello/js/main.js
@@ -1,13 +1,16 @@
+//Crear variables para 
 var btnClickInput = document.getElementById('crearLista');
 	btnClickInput.onclick =  crearLista;
 
 var crearTarjeta = document.getElementById('crearTarjeta');
 
 var createInput = document.createElement('input');
+	createInput.setAttribute('autofocus', '');
 	createInput.setAttribute('id','cajaTexto');
 
 var createSpan = document.createElement('span');
  	createSpan.setAttribute('class', 'fa fa-close');
+
 function crearLista(){
 
 	var createForm = document.createElement('div');
@@ -38,6 +41,7 @@ function presionarTache(){
 function guardar() {
 	if (createInput.value === "") {
 		createInput.setAttribute('placeholder', 'Ingresa texto válido');
+		createInput.setAttribute('autofocus', '');
 		return false;
 	} 
 
@@ -56,86 +60,78 @@ function guardar() {
 		enlaceDentroDiv.setAttribute('class', 'enlaceDentroDivTarjeta');
 
 
-	//Guardar o Concatenar todo
+	//Guardar o concatenar las variables antes creadas
 	crearTarjeta.appendChild(divTarjetaTexto);
 	divTarjetaTexto.appendChild(h4);
 	divTarjetaTexto.appendChild(enlaceDentroDiv);
 
 
-
+	//Funcion para cambiar 
 	function changeTarjetVStextarea() {
 		var divTextAreaButtonSpan = document.createElement('div');
 
 		var areaTextoCambio = document.createElement('textarea');
+			areaTextoCambio.setAttribute('autofocus', '');
 			areaTextoCambio.setAttribute('class', 'areaTextChange');
 
 		var buttonDentroDivAnadir = document.createElement('button');
 			buttonDentroDivAnadir.innerHTML="Añadir";
 			buttonDentroDivAnadir.setAttribute('class', 'btn btn-default');
 
+			divTextAreaButtonSpan.appendChild(areaTextoCambio);
+			divTextAreaButtonSpan.appendChild(buttonDentroDivAnadir);
+			divTextAreaButtonSpan.appendChild(createSpan);
 
-		divTextAreaButtonSpan.appendChild(areaTextoCambio);
-		divTextAreaButtonSpan.appendChild(buttonDentroDivAnadir);
-		divTextAreaButtonSpan.appendChild(createSpan);
+			//Remplaza el enlace <a>, por un textarea
+			enlaceDentroDiv.appendChild(divTextAreaButtonSpan);
+			enlaceDentroDiv.parentElement.replaceChild(divTextAreaButtonSpan, enlaceDentroDiv);
 
-
-		enlaceDentroDiv.appendChild(divTextAreaButtonSpan);
-		enlaceDentroDiv.parentElement.replaceChild(divTextAreaButtonSpan, enlaceDentroDiv);
-
-
+		//Funcion para validar el texto dentro del textarea y guardarlo en un div para meterlo en una Tarjetra
 		function validarTextAnadirText() {
 			if ( areaTextoCambio.value == "") {
+				areaTextoCambio.setAttribute('autofocus', '');
 				areaTextoCambio.setAttribute('placeholder', 'Ingresa texto tarjeta');
 				return false;
 				//si lo valida
 			}	
 
 		var tarjetaTextoDiv = document.createElement('div');
-			tarjetaTextoDiv.setAttribute('class', 'tarjetaTextoh3Style');
-			//tarjetaTextoDiv.setAttribute('id', 'tarjetaTextoDivId');
+			tarjetaTextoDiv.setAttribute('class', 'label-default tarjetaTextoh3Style animated swing animated');
+			tarjetaTextoDiv.setAttribute('id', 'tarjetaTextoDivId');
 			tarjetaTextoDiv.id ="tarjetaTextoDivId" + (new Date()).getTime();
 			tarjetaTextoDiv.setAttribute('draggable', 'true');
-
-			
 
 		var textoh3IncluirTarjeta = document.createElement('h3');
 			textoh3IncluirTarjeta.innerHTML=areaTextoCambio.value;
 			textoh3IncluirTarjeta.setAttribute('id', 'textoh3IncluirTarjeta');
 
-		divTarjetaTexto.appendChild(tarjetaTextoDiv);
-		tarjetaTextoDiv.appendChild(textoh3IncluirTarjeta);
+			divTarjetaTexto.appendChild(tarjetaTextoDiv);
+			tarjetaTextoDiv.appendChild(textoh3IncluirTarjeta);
 
+			//Eventos que detonan Funciones
+			divTarjetaTexto.ondragover = allowDrop;
+			divTarjetaTexto.ondrop = drop;
+			tarjetaTextoDiv.ondragstart = drag;
 
-
-		divTarjetaTexto.ondragover = allowDrop;
-		divTarjetaTexto.ondrop = drop;
-		tarjetaTextoDiv.ondragstart = drag;
-
-
-			
-
-
+		//Comenzar en 0 el input de Inicio
 		areaTextoCambio.value = "";
 		} buttonDentroDivAnadir.onclick=validarTextAnadirText;
 
-
-
-		//destino
+			//destino
 			function allowDrop(ev) {
 	    		ev.preventDefault();
-			}
-			// tarjetaTextoDiv.ondragover = allowDrop;
 
-			
+	    		this.classList.remove("animate");
+			}
 		
 			//destino
 			function drag(ev) {
 			    // ev.dataTransfer.setData("text", ev.target.id);
 			    // event.target.appendChild(document.getElementById('tarjetaTextoDivId'));
 			    ev.dataTransfer.setData("text", ev.target.id);
+			    this.style.backgroundColor = "green";
+
 			}
-			// tarjetaTextoDiv.ondrop = drag;
-			
 
 			//fuente
 			function drop(ev) {
@@ -143,23 +139,10 @@ function guardar() {
 			    var data = ev.dataTransfer.getData("text");
 	    		ev.target.appendChild(document.getElementById(data));
 
-	    		// ev.dataTransfer.setData("text", ev.target.id);
-			    // event.target.appendChild(document.getElementById('tarjetaTextoDivId'));
-			    
+	    		this.classList.add("animate");
 			}
-			// tarjetaTextoDiv.ondragstart = drop;
-
-
-
-
-
 
 	} enlaceDentroDiv.onclick = changeTarjetVStextarea;
-
-	
-
-
-
 
 	//Comenzar en 0 el input de Inicio
 	createInput.value = "";
